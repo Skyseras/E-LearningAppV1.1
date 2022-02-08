0d2d738117f865d250e40334b4811aaaa6965105
Yasser <93922305+Skyseras@users.noreply.github.com>
2022-02-08 09:29:22 +0100

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@
         <a href="#" class="px-4 py-1 list-group-item-action bg-transparent second-text fw-bold"><button class="w-100 btn1 text-black fs-6 fw-bold " type="submit"><img class="mleft px-1 pb-1" src="icons/Settings.svg" alt="">&nbsp;&nbsp;Settings</button></a>
     </div>
     <div class="text-left list-group list-group-flush my-3">
-        <a href="#" class="px-4 py-1 list-group-item-action bg-transparent second-text fw-bold"><button class="w-100 btn1 text-black fs-6 fw-bold pleft" type="submit">Logout&nbsp;&nbsp;&nbsp;&nbsp;<img class="pb-1" src="icons/logout.svg" alt=""></button></a>
+        <a href="signin.php" class="px-4 py-1 list-group-item-action bg-transparent second-text fw-bold"><button class="w-100 btn1 text-black fs-6 fw-bold pleft" type="submit">Logout&nbsp;&nbsp;&nbsp;&nbsp;<img class="pb-1" src="icons/logout.svg" alt=""></button></a>
     </div>
 </div>
 <!-- /#sidebar-wrapper -->
